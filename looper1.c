d3fbedb86f630ca1b1a5c927b93453e2cc58ba0f
RATHOD122006 <sumitrathod8225@gmail.com>
2024-06-26 10:50:42 +0530

    looper

---
+++ looper1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+
+{
+	
+    char ch = 'a';
+    
+     
+    
+    do{
+    	
+    printf("%c\n", ch);
+    
+        ch+=4;
+        
+		    }while(ch<'z');
+
+
+}
+    
